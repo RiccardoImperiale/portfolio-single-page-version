13608d29b77f79402bd2700cc62e6b4882e758ee
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-30 11:55:07 +0200

    fix header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import CustomCursor from './components/CustomCursor.vue'
+import AppHero from './components/AppHero.vue'
+import SectionAbout from './components/SectionAbout.vue'
+import SectionWorks from './components/SectionWorks.vue'
+import AnimationEnd from './components/AnimationEnd.vue'
+import SectionContact from './components/SectionContact.vue'
 
 export default {
     name: 'App',
     components: {
         AppHeader,
-        CustomCursor
+        CustomCursor,
+        AppHero,
+        SectionAbout,
+        SectionWorks,
+        AnimationEnd,
+        SectionContact
     },
 }
 </script>
 
 <template>
     <AppHeader />
-    <main>
-        <RouterView />
-    </main>
+    <AppHero />
+    <SectionAbout />
+    <AnimationEnd />
+    <SectionWorks />
+    <SectionContact />
+
+
     <CustomCursor />
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,110 +4,93 @@ import { animateHeader } from '../assets/gsap/animations.js';
 
 export default {
     name: 'AppHeader',
-    props: {
-        color: String,
-    },
     data() {
         return {
-            store,
-            isNavWhite: null,
+            store
         }
     },
+    methods: {
+        isOver(val) {
+            store.isCustomCursor = val
+        },
+    },
     mounted() {
-        const header = this.$refs.header;
+        // const header = this.$refs.header;
+        const header = document.querySelector('header')
         const links = document.querySelectorAll('.link_container')
 
         animateHeader(header, links);
-
-        this.$watch(() => this.$route, (to) => {
-            if (to.name === 'home' || to.name === 'contact') {
-                this.isNavWhite = true;
-            } else {
-                this.isNavWhite = false;
-            }
-        });
-    }
+    },
 }
 </script>
 
 <template>
-    <header :class="isNavWhite || 'invert'" ref="header" @mouseover="store.isCursorHidden = true"
-        @mouseleave="store.isCursorHidden = false">
-        <nav ref="nav">
-            <router-link class="nav_link" :to="{ name: 'home' }">
-                <img class="logo" width=35 src="/img/logo-wt.png" alt="logo">
-            </router-link>
+    <header ref="header" @mouseover="store.isCursorHidden = true" @mouseleave="store.isCursorHidden = false">
+        <nav>
+            <img class="logo" width=35 src="/img/logo-wt.png" alt="logo">
             <div class="page_links">
-                <router-link class="link_container" :to="{ name: 'home' }">
-                    <div class="nav_link">HOME</div>
-                    <div class="nav_link">HOME</div>
-                </router-link>
-                <!-- <router-link class="link_container" :to="{ name: 'about' }">
-                    <div class="nav_link">ABOUT</div>
-                    <div class="nav_link">ABOUT</div>
-                </router-link> -->
-                <router-link class="link_container" :to="{ name: 'projects' }">
-                    <div class="nav_link">ALL PROJECTS</div>
-                    <div class="nav_link">ALL PROJECTS</div>
-                </router-link>
-                <router-link :class="isNavWhite || 'invert'" class="pill" :to="{ name: 'contact' }">
-                    <div class="nav_link">CONTACT</div>
-                    <div class="nav_link">CONTACT</div>
-                </router-link>
-                <!-- <a href="#contact" :class="isNavWhite || 'invert'" class="pill">
-                    <div class="nav_link">CONTACT</div>
-                    <div class="nav_link">CONTACT</div>
-                </a> -->
+                <div class="link_container">
+                    <a href="#about">ABOUT</a>
+                    <a href="#about">ABOUT</a>
+                </div>
+                <div class="link_container">
+                    <a href="#works">WORKS</a>
+                    <a href="#works">WORKS</a>
+                </div>
+                <div class="link_container pill">
+                    <a href="#contact">CONTACT</a>
+                    <a href="#contact">CONTACT</a>
+                </div>
             </div>
         </nav>
     </header>
 </template>
 
 <style scoped>
-.invert {
-    filter: invert();
-}
-
-.nav_link {
-    color: var(--pf-lighter);
-    font-size: .9rem;
-    opacity: 0.5;
-    transition: .3s ease;
-}
-
 header {
     position: absolute;
-    position: fixed;
     top: 0;
     width: 100%;
-    z-index: 1;
+    z-index: 10;
 
     & nav {
         max-width: 1920px;
         width: 90%;
         margin: auto;
-        padding: 1rem 0;
+        padding: 1.5rem 0;
         display: flex;
         justify-content: space-between;
         align-items: center;
 
         .logo {
             mix-blend-mode: color-dodge;
-            opacity: 0.9;
+            opacity: 0.6;
         }
 
         .page_links {
             display: flex;
-            gap: 1.5rem;
             align-items: center;
+            gap: 1.6rem;
+
+            a {
+                display: block;
+                text-decoration: none;
+                color: var(--pf-gray-100);
+                font-size: .9rem;
+                opacity: 0.7;
+                transition: .3s ease;
+            }
 
             .link_container {
                 overflow: hidden;
                 height: 17px;
-                text-decoration: none;
 
-                &:hover .nav_link {
-                    transform: translateY(-17px);
+                &:hover a {
+                    transform: translateY(-20px);
+                }
+
+                a:nth-of-type(2) {
+                    padding-top: 2px;
                 }
             }
 
@@ -118,14 +101,19 @@ header {
                 overflow: hidden;
                 height: 20px;
                 text-decoration: none;
+                display: flex;
+                flex-direction: column;
 
-                &:hover .nav_link {
+                &:hover a {
                     transform: translateY(-35px);
                 }
 
-                .nav_link {
+                a {
                     transform: translateY(-8px);
-                    margin-bottom: 10px;
+                }
+
+                a:nth-of-type(2) {
+                    padding-top: 10px;
                 }
             }
         }
